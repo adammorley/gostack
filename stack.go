1f2f2f5c20797ea565909264992abf89b362d0c2
Adam Morley <adam@Adams-Mac-mini.local>
2018-08-15 09:45:31 -0700

    gofmt

--- stack.go
+++ stack.go
@@ -1,5 +1,5 @@
 /*
-    a stack implementation for stuff (using interface{}) in golang
+   a stack implementation for stuff (using interface{}) in golang
 */
 
 package stack
@@ -9,65 +9,66 @@ import "reflect"
 
 // a stack is an in-order pile of things; in this particular case, whatever was last added is on the top.
 type Stack struct {
-    top *element
-    count uint
+	top   *element
+	count uint
 }
 
 // an error can be returned
 type StackError struct {
-    msg string
+	msg string
 }
+
 func (e *StackError) Error() string {
-    return fmt.Sprintf("stack %v", e.msg)
+	return fmt.Sprintf("stack %v", e.msg)
 }
 
 type element struct {
-    value interface{}
-    under *element
+	value interface{}
+	under *element
 }
 
 // create a new stack
 func New() *Stack {
-    var s *Stack = new(Stack)
-    s.top = nil
-    return s
+	var s *Stack = new(Stack)
+	s.top = nil
+	return s
 }
 
 // return the number of elements on the stack
 func (s *Stack) Count() uint {
-    return s.count
+	return s.count
 }
 
 // peek at the element on the top of the stack
 func (s *Stack) Peek() (interface{}, error) {
-    var e *element = s.top
-    if e == nil {
-        return 0, &StackError{msg: "empty"}
-    }
-    return e.value, nil
+	var e *element = s.top
+	if e == nil {
+		return 0, &StackError{msg: "empty"}
+	}
+	return e.value, nil
 }
 
 // pop an element from the top of the stack
 func (s *Stack) Pop() (interface{}, error) {
-    var e *element = s.top
-    if e == nil {
-        return 0, &StackError{msg: "empty"}
-    }
-    s.top = e.under
-    s.count -= 1
-    return e.value, nil
+	var e *element = s.top
+	if e == nil {
+		return 0, &StackError{msg: "empty"}
+	}
+	s.top = e.under
+	s.count -= 1
+	return e.value, nil
 }
 
 // push an element on the top of the stack
 // enforces that the types match as elements are pushed
 func (s *Stack) Push(i interface{}) error {
-    if s.top != nil && reflect.TypeOf(s.top.value) != reflect.TypeOf(i) {
-        return &StackError{msg: "type mismatch"}
-    }
-    var e *element = new(element)
-    e.value = i
-    e.under = s.top
-    s.top = e
-    s.count += 1
-    return nil
+	if s.top != nil && reflect.TypeOf(s.top.value) != reflect.TypeOf(i) {
+		return &StackError{msg: "type mismatch"}
+	}
+	var e *element = new(element)
+	e.value = i
+	e.under = s.top
+	s.top = e
+	s.count += 1
+	return nil
 }
--- stack_test.go
+++ stack_test.go
@@ -3,21 +3,21 @@ package stack
 import "testing"
 
 func TestStack(t *testing.T) {
-    var s *Stack = New()
-    s.Push(1)
-    s.Push(2)
-    s.Push(3)
-    e := s.Push("foo")
-    if _, ok := e.(*StackError); !ok {
-        t.Error("did not get back an error when pushing incompatible type", e)
-    }
-    if s.Count() != 3 {
-        t.Error("pushed three, didn't find them")
-    } else if v, e := s.Pop(); e != nil || v != 3 {
-        t.Error("expected element not popped")
-    } else if v, e := s.Peek(); e != nil || v != 2 {
-        t.Error("peek broken")
-    } else if s.Count() != 2 {
-        t.Error("count broken.")
-    }
+	var s *Stack = New()
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	e := s.Push("foo")
+	if _, ok := e.(*StackError); !ok {
+		t.Error("did not get back an error when pushing incompatible type", e)
+	}
+	if s.Count() != 3 {
+		t.Error("pushed three, didn't find them")
+	} else if v, e := s.Pop(); e != nil || v != 3 {
+		t.Error("expected element not popped")
+	} else if v, e := s.Peek(); e != nil || v != 2 {
+		t.Error("peek broken")
+	} else if s.Count() != 2 {
+		t.Error("count broken.")
+	}
 }
